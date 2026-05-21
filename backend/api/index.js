fab04f11a409e224338f56f0e4b331b0a31e24ad
joahn288-create <johanmartinez288@gmail.com>
2026-05-20 20:17:45 -0500

    fix: reescribir rutas en index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,6 +1,11 @@
-const app = require('../server');
+const server = require('../server');
+const express = require('express');
+const app = express();
 
-// Reescribir rutas quitando /api
-const originalStack = app._router.stack;
+// Reenviar todas las rutas agregando /api al inicio
+app.use('/', (req, res, next) => {
+  req.url = '/api' + req.url;
+  server(req, res, next);
+});
 
 module.exports = app;
